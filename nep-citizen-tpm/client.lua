a7a4f08539f8e636cf064557e7c10bc8a8a6aba1
SynTer <alpacanismo@gmail.com>
2022-01-12 20:06:45 -0300

    refactor(client.lua): Code refactored

--- nep-citizen-tpm/client.lua
+++ nep-citizen-tpm/client.lua
@@ -1,5 +1,5 @@
-consoleRem = true -- enabled by default.
-chatRem = false -- disabled by default.
+local consoleRem = true -- enabled by default.
+local chatRem = false -- disabled by default.
 
 TriggerEvent('chat:addSuggestion', '/tpm', "Teleport to your marked waypoint.")
 
@@ -9,27 +9,24 @@ end)
 
 TeleportToWaypoint = function()
     local WaypointHandle = GetFirstBlipInfoId(8)
+    local pid = PlayerPedId()
     if DoesBlipExist(WaypointHandle) then
         local waypointCoords = GetBlipInfoIdCoord(WaypointHandle)
         for height = 1, 1000 do
-            SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+            SetPedCoordsKeepVehicle(pid, waypointCoords["x"], waypointCoords["y"], height + 0.0)
             local foundGround, zPos = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
             if foundGround then
-                SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+                SetPedCoordsKeepVehicle(pid, waypointCoords["x"], waypointCoords["y"], height + 0.0)
                 break
             end
             Wait(5)
         end
-    else
-        if chatRem then
-            TriggerEvent('chat:addMessage', {
-                color = {255,85,85},
-                args = {"citizen-tpm", "client needs to set a waypoint first."}
-            })
-        else
-            if consoleRem then
-                print("client needs to set a waypoint first")
-            end
-        end
+    elseif ( chatRem ) then
+        TriggerEvent('chat:addMessage', {
+            color = {255,85,85},
+            args = {"citizen-tpm", "client needs to set a waypoint first."}
+        })
+    elseif ( consoleRem ) then
+        print("client needs to set a waypoint first")
     end
 end
