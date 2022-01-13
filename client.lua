6a8dd9266a2fbff9cdcc5527ac8e12e20ee73b0e
Bombay <OMGmsebastian@gmail.com>
2022-01-13 12:55:56 -0500

    feat(client): changed the way tp works

--- client.lua
+++ client.lua
@@ -4,34 +4,53 @@ local commandName <const> = 'tpm'
 
 local function teleportToWaypoint()
     local blipMarker <const> = GetFirstBlipInfoId(8)
-    local ped = PlayerPedId()
     if DoesBlipExist(blipMarker) then
-        local waypointCoords = GetBlipInfoIdCoord(blipMarker)
-        for height = 1, 1000 do
-            SetPedCoordsKeepVehicle(ped, waypointCoords["x"], waypointCoords["y"], height + 0.0)
-            local foundGround, _ = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
-            if foundGround then
-                SetPedCoordsKeepVehicle(ped, waypointCoords["x"], waypointCoords["y"], height + 0.0)
+        local coords, found, zState = GetBlipInfoIdCoord(blipMarker), true, -1.0
+        local x, z, y = coords['x'], coords['y'], 1500.0
+
+        SetTimeout(function() if found then found = false end end, 5000)
+        while found do
+            local groundFound, groundZ = GetGroundZFor_3dCoord(x + 0.0, y + 0.0, z + zState)
+            if z < 0 then
+                zState = 1.0
+            elseif z > 1500 then
+                zState = -1.0
+            end
+            if groundFound then
+                z = groundZ + 0.0
+                SetPedCoordsKeepVehicle(PlayerPedId(), x, y, z)
                 return true
             end
-            Wait(5)
+            Wait(0)
         end
-    else
-        if chatRem then
-            TriggerEvent('chat:addMessage', {
-                color = {255,85,85},
-                args = {"citizen-tpm", "client needs to set a waypoint first."}
-            })
-        end
-        if consoleRem then
-            print("client needs to set a waypoint first")
-        end
-        return false
     end
+    return false
 end
 
 RegisterCommand(commandName, function()
-    teleportToWaypoint()
+    local hasTeleported <const> = teleportToWaypoint()
+    if not hasTeleported then
+        if chatRem then
+            TriggerEvent('chat:addMessage', {
+                color = {255,85,85},
+                args = {"citizen-tpm", "could not find ground."}
+            })
+        end
+        if consoleRem then
+            print("could not find ground")
+        end
+        return
+    end
+
+    if chatRem then
+        TriggerEvent('chat:addMessage', {
+            color = {255,85,85},
+            args = {"citizen-tpm", "client needs to set a waypoint first."}
+        })
+    end
+    if consoleRem then
+        print("client needs to set a waypoint first")
+    end
 end)
 
 TriggerEvent('chat:addSuggestion', '/' .. commandName, "Teleport to your marked waypoint.")
